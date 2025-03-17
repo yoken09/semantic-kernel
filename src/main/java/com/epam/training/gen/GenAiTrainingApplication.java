db59bd26845a168a4b7b888699fb57cae54dbb61
ykeni <yogesh_keni@epam.com>
2025-03-17 13:11:47 +0530

    Initial changes

---
+++ src/main/java/com/epam/training/gen/GenAiTrainingApplication.java
@@ -0,0 +1,15 @@
+package com.epam.training.gen.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:/config/application.properties")
+public class GenAiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiTrainingApplication.class, args);
+	}
+
+}
