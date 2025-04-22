64f3968b1d9a9325760a02a0affe7bb1b311fec8
ykeni <yogesh_keni@epam.com>
2025-04-22 11:24:45 +0530

    Added postal code plugin

--- src/main/java/com/epam/training/gen/ai/controller/SemanticKerneliController.java
+++ src/main/java/com/epam/training/gen/ai/controller/SemanticKerneliController.java
@@ -2,17 +2,31 @@ package com.epam.training.gen.ai.controller;
 
 import com.azure.core.exception.HttpResponseException;
 import com.epam.training.gen.ai.model.request.PromptRequest;
+import com.epam.training.gen.ai.model.request.UserRequest;
 import com.epam.training.gen.ai.model.response.ChatResponse;
+import com.epam.training.gen.ai.service.NavigationService;
+import com.epam.training.gen.ai.service.PostalPluginService;
 import com.epam.training.gen.ai.service.SemanticKernelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1")
 public class SemanticKerneliController {
     @Autowired
     SemanticKernelService semanticKernelService;
+
+    @Autowired
+    PostalPluginService postalPluginService;
+
+    private static final String INPUT_KEY = "input";
+    private static final String TEMPERATURE_KEY = "temperature";
 
     @GetMapping("/generateContent")
     public ChatResponse generateContent(@RequestParam String prompt)  {
@@ -39,5 +53,16 @@ public class SemanticKerneliController {
         } catch (HttpResponseException exception) {
             throw new RuntimeException();
         }
+    }
+
+    @PostMapping("/postal/plugin")
+    public Mono<ResponseEntity<Map<String, Object>>> processWithPlugin(@RequestParam String request) {
+        if (request == null || request.isEmpty()) {
+            return Mono.just(ResponseEntity.badRequest().body(Map.of("error", "Input cannot be empty")));
+        }
+        return Mono.fromCallable(() -> {
+            List<String> result = postalPluginService.processWithModels(request);
+            return ResponseEntity.ok(Map.of("response:", Objects.requireNonNull(result)));
+        });
     }
 }
