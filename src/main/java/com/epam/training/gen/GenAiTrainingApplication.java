2336c269e3ebcfe2207a1b9c829266c8da397149
ykeni <yogesh_keni@epam.com>
2025-04-29 02:07:05 +0530

    Revert "Changes for gen-ai task5 embeddings task"
    This reverts commit 20410c043b4cafd6473162fd72996ca8548788f8.

--- src/main/java/com/epam/training/gen/GenAiTrainingApplication.java
+++ src/main/java/com/epam/training/gen/GenAiTrainingApplication.java
@@ -2,12 +2,8 @@ package com.epam.training.gen.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
-@EnableConfigurationProperties
-@ConfigurationPropertiesScan
 @SpringBootApplication
 @PropertySource("classpath:/config/application.properties")
 public class GenAiTrainingApplication {
